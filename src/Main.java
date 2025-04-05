2c44d58a3d2250d09ac7cd901c821c453973003c
LuanBertozzi <LuanBertozziS2@hotmail.com>
2025-04-05 14:12:10 -0400

    sem mudanças

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("olá mundo");
+        while(true){
+            System.out.println("" +
+                    "### Calculadora ###" +
+                    "(1) Calculadora Matemática" +
+                    "(2) IMC" +
+                    "(3) ???" +
+                    "(4) ???");
+        }
     }
 }
