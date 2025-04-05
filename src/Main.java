e3558bde4de58742ac88d52e30accd664020f0f1
LuanBertozzi <LuanBertozziS2@hotmail.com>
2025-04-05 14:21:46 -0400

    Adicionado exemplo de como vai funcionar

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        short escolhaUsuario;
             System.out.println("" +
                     "### Calculadora ###" +
                     "(1) Calculadora Matemática" +
                     "(2) IMC" +
                     "(3) ???" +
                     "(4) ???");
+            escolhaUsuario = scanner.nextShort();
+            if(escolhaUsuario == 1){
+                // vamo chamar a classe calculadora
+            }
     }
 }
