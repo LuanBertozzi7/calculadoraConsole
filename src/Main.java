88ed361cc63bbd938d01007c7cb840e31a4f8c9e
LuanBertozzi <LuanBertozziS2@hotmail.com>
2025-04-05 14:16:15 -0400

    while removido

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        while(true){
             System.out.println("" +
                     "### Calculadora ###" +
                     "(1) Calculadora Matemática" +
                     "(2) IMC" +
                     "(3) ???" +
                     "(4) ???");
-        }
     }
 }
